f7c4302cab82015133f75caf2acc11523424367d
Liuban0Stanislav <liubansytanislav@yandex.ru>
2024-03-02 13:30:45 +0300

    init commit

---
+++ src/main/java/com/_lightdigitaltask/LightDigitalTaskApplication.java
@@ -0,0 +1,13 @@
+package com._lightdigitaltask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightDigitalTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightDigitalTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/_lightdigitaltask/LightDigitalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com._lightdigitaltask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightDigitalTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
